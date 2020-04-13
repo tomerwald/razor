b2cf13d139937044b23800166a92ef4f57f47545
Tomer Waldman <defsaf9@gmail.com>
2020-04-13 15:42:34 +0300

    Added protocol consts

---
+++ peer_protocol/protocol.go
@@ -0,0 +1,37 @@
+package peer_protocol
+
+const (
+	Protocol = "\x13BitTorrent protocol"
+)
+
+const (
+	// BEP 3
+	Choke         byte = 0
+	Unchoke       byte = 1
+	Interested    byte = 2
+	NotInterested byte = 3
+	Have          byte = 4
+	Bitfield      byte = 5
+	Request       byte = 6
+	Piece         byte = 7
+	Cancel        byte = 8
+	Port          byte = 9
+
+	// BEP 6 - Fast extension
+	Suggest     byte = 0x0d // 13
+	HaveAll     byte = 0x0e // 14
+	HaveNone    byte = 0x0f // 15
+	Reject      byte = 0x10 // 16
+	AllowedFast byte = 0x11 // 17
+
+	// BEP 10
+	Extended byte = 20
+)
+
+const (
+	HandshakeExtendedID = 0
+
+	RequestMetadataExtensionMsgType = 0
+	DataMetadataExtensionMsgType    = 1
+	RejectMetadataExtensionMsgType  = 2
+)
