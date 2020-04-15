dab2b7d1baf2f06c23aff814e12c06c150a8df52
Tomer Waldman <defsaf9@gmail.com>
2020-04-15 14:23:43 +0300

    Added full handshake capability

--- main.go
+++ main.go
@@ -1,27 +1,33 @@
 package main
 
 import (
+	"./config"
 	"./peer_protocol"
 	"fmt"
 	"net"
 )
 
-func handleClient(con net.Conn) {
-	InfoHash := []byte{0xd5, 0x5f, 0x1e, 0x84, 0x0f, 0x1b, 0xd6,
-		0x57, 0x6e, 0xad, 0x67, 0xa4, 0xd0, 0x4e, 0x5d, 0x6e, 0xa2, 0x94, 0x41, 0x4b}
-	PeerID := []byte("-UW109K-LMYpj9A)8X0R")
-	fmt.Println("Handling client")
-	p := peer_protocol.PeerConnection{
-		PeerID:   PeerID,
-		InfoHash: InfoHash,
-		Conn:     con,
-	}
-	p.Handshake()
+type PeerManger struct {
+	Config config.RazorConifg
 }
 
-func connection_loop() {
-	fmt.Println("Getting connections")
-	l, err := net.Listen("tcp", "127.0.0.1:6888")
+func (pm PeerManger) handleClient(con net.Conn) {
+	fmt.Printf("Handling client %s\r\n", con.RemoteAddr().String())
+	p := peer_protocol.PeerConnection{
+		PeerID:   pm.Config.PeerID,
+		InfoHash: pm.Config.InfoHash,
+		Conn:     con,
+	}
+	if p.PerformHandshake() {
+		p.Choke()
+	} else {
+
+	}
+}
+
+func (pm PeerManger) Start() {
+	fmt.Println("Getting connections\r\n")
+	l, err := net.Listen("tcp", pm.Config.Address)
 	if err != nil {
 		fmt.Println("Failed listening for connections")
 		return
@@ -31,10 +37,19 @@ func connection_loop() {
 			if err != nil {
 				continue
 			}
-			go handleClient(conn)
+			go pm.handleClient(conn)
 		}
 	}
 }
 func main() {
-	connection_loop()
+	InfoHash := []byte{0xd5, 0x5f, 0x1e, 0x84, 0x0f, 0x1b, 0xd6,
+		0x57, 0x6e, 0xad, 0x67, 0xa4, 0xd0, 0x4e, 0x5d, 0x6e, 0xa2, 0x94, 0x41, 0x4b}
+	PeerID := []byte("-UW109K-LMYpj9A)8X0R")
+	c := config.RazorConifg{
+		Address:  "127.0.0.1:6888",
+		InfoHash: InfoHash,
+		PeerID:   PeerID,
+	}
+	pm := PeerManger{c}
+	pm.Start()
 }
--- peer_protocol/peer.go
+++ peer_protocol/peer.go
@@ -1,27 +1,29 @@
 package peer_protocol
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
 	"net"
 )
 
 type PeerConnection struct {
-	Conn     net.Conn
-	PeerID   []byte
-	InfoHash []byte
+	Conn         net.Conn
+	PeerID       []byte
+	InfoHash     []byte
+	remotePeerID []byte
 }
 
-func (p PeerConnection) CreateMessageBuffer(mt byte, payload []byte) []byte {
+func (p PeerConnection) createMessageBuffer(mt byte, payload []byte) []byte {
 	LengthField := make([]byte, 4)
 	binary.BigEndian.PutUint32(LengthField, uint32(len(payload)+1))
 	ActionField := []byte{mt}
-	PrefixBuffer := append(ActionField, LengthField...)
+	PrefixBuffer := append(LengthField, ActionField...)
 	buffer := append(PrefixBuffer, payload...)
 	return buffer
 }
 
-func (p PeerConnection) Handshake() {
+func (p PeerConnection) SendHandshake() {
 	fmt.Println("Handshaking")
 	protocol_type := []byte(Protocol)          // const
 	reserved := []byte{0, 0, 0, 0, 0, 0, 0, 0} // all zero 8 bytes
@@ -29,6 +31,51 @@ func (p PeerConnection) Handshake() {
 	buf = append(buf, p.PeerID...)
 	buf = append(buf, p.InfoHash...)
 	l, err := p.Conn.Write(buf)
-	if err != nil{}
+	if err != nil {
+	}
 	fmt.Println(l)
+}
+
+func (p PeerConnection) IsHandshakeValid(protocol string, hash []byte) bool {
+	if protocol != protocol {
+		return false
+	} else if bytes.Compare(hash, p.InfoHash) == 0 {
+		return false
+	} else {
+		return true
+	}
+}
+
+func (p PeerConnection) ReceiveHandshake() bool {
+	fmt.Println("Handshaking")
+	buf := make([]byte, 68)
+	_, err := p.Conn.Read(buf)
+	if err != nil {
+		fmt.Println("Failed to read bytes from socket")
+	}
+	proto := string(buf[:20])
+	p.remotePeerID = buf[27:47]
+	InfoHash := buf[47:67]
+	if p.IsHandshakeValid(proto, InfoHash) {
+		fmt.Printf("Peer id: %s\r\n", string(p.remotePeerID))
+		return true
+	} else {
+		return false
+	}
+}
+
+func (p PeerConnection) Choke() error {
+	var payload []byte
+	buf := p.createMessageBuffer(Choke, payload)
+	_, err := p.Conn.Write(buf)
+	return err
+}
+
+func (p PeerConnection) PerformHandshake() bool{
+	if p.ReceiveHandshake() {
+		p.SendHandshake()
+		return true
+	} else {
+		return false
+	}
 }
